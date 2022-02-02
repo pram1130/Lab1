c508fdfabbb454eaabf9e02c6ee8734a10901ebf
MSI X <MSI X@DESKTOP-IQUTKIK>
2022-02-02 21:54:38 +0700

    LAB1

---
+++ LAB1/src/LAB1/main.java
@@ -0,0 +1,14 @@
+package LAB1;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Wolrd");
+	}
+
+}
